de4f52dc35cdbda035345d27ed9b59d3ae3a8f04
Vikash Sharma <expertvikash@gmail.com>
2025-01-04 21:01:20 +0530

    refactor store modal to enhance form handling and add cancel and continue with actions button

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -5,49 +5,57 @@ import { useForm } from "react-hook-form";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Model } from "../ui/model";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const formSchema = z.object({
-  name:z.string().min(1)
-})
+  name: z.string().min(1),
+});
 
 export const StoreModel = () => {
-    const storeModel=useStoreModal();
-    const form =useForm<z.infer<typeof formSchema>>({
-      resolver:zodResolver(formSchema),
-      defaultValues:{
-        name:""
-      }
-    })
-    const onSubmit = async(values:z.infer<typeof formSchema>)=>{
-      console.log(values)
-    }
-    return <Model
-    title="Create store"
-    description="Add a new store to manage products and categories"
-    isOpen={storeModel.isOpen}
-    onClose={storeModel.onClose}
-  >
-    <div>
-    <div className="space-y-4 py-2 pb-4">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <FormField control={form.control} name="name" render={({field})=>(
-<FormItem>
-  <FormLabel>Name</FormLabel>
-  <FormControl>
-
-  <Input placeholder="store" {...field}/>
-  </FormControl>
-
-</FormItem>
-          )}/>
-
-        </form>
-      </Form>
-    </div>
-
-    </div>
-  </Model>;
+  const storeModel = useStoreModal();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+  return (
+    <Model
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModel.isOpen}
+      onClose={storeModel.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Store" {...field} />
+                    </FormControl>
+                    <FormMessage/>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full ">
+                <Button variant={"outline"} onClick={storeModel.onClose}>Cancel</Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Model>
+  );
 };
